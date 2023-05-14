768ae402fa21d0845b8a82189f121fb6e4789087
piupaulgit <contactpiu619@gmail.com>
2023-05-15 00:17:49 +0530

    single page

---
+++ plantmore/src/components/OtherPageBanner.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OtherPageBanner = () => {
+  return (
+    <section className='py-10 bg-gray-100 mb-10'>
+        <div className='container mx-auto flex justify-between'>
+            <h2 className=' font-bold text-3xl text-gray-800'>Shop</h2>
+        </div>
+    </section>
+  )
+}
+
+export default OtherPageBanner
