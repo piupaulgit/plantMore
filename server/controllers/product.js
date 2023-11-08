aaa807d40df01b3c95fd5090d7341536e2e6ee20
piupaulgit <contactpiu619@gmail.com>
2023-11-08 18:54:50 +0530

    other product apis

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -2,6 +2,26 @@ const Product = require("../models/product");
 const formidable = require("formidable");
 const fs = require("fs");
 
+exports.getProductById = async (req, res, next, id) => {
+  try {
+    const product = await Product.findById(id);
+    req.product = product;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getProduct = (req, res) => {
+  req.product.photo = undefined;
+  res.status(200).json({
+    status: "success",
+    data: {
+      product: req.product,
+    },
+  });
+}
+
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
@@ -18,7 +38,6 @@ exports.addProduct = async (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
 
-    console.log(req.body,'ppppppppppp')
   form.parse(req, async (err, fields, file) => {
     if (err) {
       return res.status(400).json({
@@ -37,29 +56,47 @@ exports.addProduct = async (req, res) => {
       name: name[0],
       category: category[0],
       price: price[0],
-      stock: stock[0]
-    }
+      stock: stock[0],
+    };
 
     let product = await Product.create(obg);
 
     if (file.photo) {
-        if (file.photo.size > 3000000) {
-          return res.status(400).json({
-            error: "File size too big",
-          });
-        }
-        console.log(file.photo)
-        product.photo.data = fs.readFileSync(file.photo[0].filepath);
-        product.photo.contentType = file.photo[0].mimetype;
-      }
-
-      try {
-        const savedProduct = await product.save(); 
-        res.json(savedProduct);
-      } catch (err) {
+      if (file.photo.size > 3000000) {
         return res.status(400).json({
-          error: err.message, 
+          error: "File size too big",
         });
       }
+
+      product.photo.data = fs.readFileSync(file.photo[0].filepath);
+      product.photo.contentType = file.photo[0].mimetype;
+    }
+
+    try {
+      const savedProduct = await product.save();
+      res.json(savedProduct);
+    } catch (err) {
+      return res.status(400).json({
+        error: err.message,
+      });
+    }
   });
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+  } catch (err) {}
+};
+
+exports.deleteProduct = async(req, res) => {
+  let product = req.product;
+  try{
+    await Product.deleteOne({ "_id" :  product._id});
+    res.status(201).json({
+      status: "success",
+      message: "Product Deleted Successfully"
+    });
+  }catch(err){
+    console.log(err)
+  }
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
-const { getAllProducts, addProduct } = require("../controllers/product");
+const { getAllProducts, addProduct, getProductById, updateProduct, deleteProduct, getProduct } = require("../controllers/product");
 const { getUserById } = require("../controllers/user");
 const router = express.Router();
 
 router.param("userId", getUserById);
+router.param("productId", getProductById);
 
 router.get("/all", getAllProducts);
+router.get("/:userId/:productId", isSignedIn, isAuthenticated, isAdmin, getProduct);
 router.post("/add/:userId", isSignedIn, isAuthenticated, isAdmin, addProduct);
+router.put("/update/:userId/:productId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+router.delete("/delete/:userId/:productId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
 
 module.exports = router;
