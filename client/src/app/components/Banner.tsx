f032fb6ce1832ccd39fa3f065d1918449d3db397
piupaulgit <contactpiu619@gmail.com>
2024-01-03 22:26:12 +0530

    banner updated

--- client/src/app/components/Banner.tsx
+++ client/src/app/components/Banner.tsx
@@ -15,14 +15,37 @@ interface IBanner {
 }
 
 const Banner = () => {
-  const [banners, setBanners] = useState<IBanner[]>();
+  const [bannerOne, setBannerOne] = useState<IBanner>(Object);
+  const [bannerTwo, setBannerTwo] = useState<IBanner>(Object);
+  const [bannerThree, setBannerThree] = useState<IBanner>(Object);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     setIsLoading(true);
     try {
       getAllBanners().then((res: any) => {
-        setBanners(res.data.banners);
+        res.data.banners.forEach((banner: IBanner) => {
+          const bannerObject: IBanner = {
+            title: banner.title,
+            subTitle: banner.subTitle,
+            image: banner.image,
+            type: banner.type,
+          };
+
+          switch (bannerObject.type) {
+            case "banner_one":
+              setBannerOne(bannerObject);
+              break;
+
+            case "banner_two":
+              setBannerTwo(bannerObject);
+              break;
+
+            case "banner_three":
+              setBannerThree(bannerObject);
+              break;
+          }
+        });
         setIsLoading(false);
       });
     } catch {}
@@ -35,53 +58,58 @@ const Banner = () => {
           isLoading && "min-h-[500px] bg-gray-100 relative"
         }`}
       >
+        {}
         {isLoading && <Spinner />}
-        {banners && (
-          <>
+        <>
+          {bannerOne?.title && (
             <div className="w-[49%] relative">
               <ImageHelper
-                imageAlt={banners[0].title}
-                imageData={banners[0].image}
+                imageAlt={bannerOne.title}
+                imageData={bannerOne.image}
               />
               <div className="text-white text-center absolute top-[80px] left-[50%] translate-x-[-50%] w-full">
                 <h2 className=" font-extralight text-2xl mb-3">
-                  {banners[0].title}
+                  {bannerOne.title}
                 </h2>
-                <h1 className=" font-bold text-6xl">{banners[0].subTitle}</h1>
+                <h1 className=" font-bold text-6xl">{bannerOne.subTitle}</h1>
               </div>
             </div>
-            <div className="flex flex-col gap-10 w-[51%]">
+          )}
+          <div className="flex flex-col gap-10 w-[51%]">
+            {bannerTwo?.title && (
               <div className=" relative">
                 <ImageHelper
-                  imageAlt={banners[1].title}
-                  imageData={banners[1].image}
+                  imageAlt={bannerTwo.title}
+                  imageData={bannerTwo.image}
                 />
                 <div className="absolute right-[20px] top-[20%] max-w-[40%]">
                   <h2 className="text-4xl font-black text-gray-700 mb-2">
-                    {banners[1].title}
+                    {bannerTwo.title}
                   </h2>
                   <h4 className="text-3xl font-light text-gray-700">
-                    {banners[1].subTitle}
+                    {bannerTwo.subTitle}
                   </h4>
                 </div>
               </div>
+            )}
+            {bannerThree?.title && (
               <div className=" relative">
                 <ImageHelper
-                  imageAlt={banners[2].title}
-                  imageData={banners[2].image}
+                  imageAlt={bannerThree.title}
+                  imageData={bannerThree.image}
                 />
                 <div className=" absolute left-[40px] top-[20%] max-w-[40%]">
                   <h2 className="text-4xl font-black text-gray-700 mb-2">
-                    {banners[2].title}
+                    {bannerThree.title}
                   </h2>
                   <h4 className="text-3xl font-light text-gray-700">
-                    {banners[2].subTitle}
+                    {bannerThree.subTitle}
                   </h4>
                 </div>
               </div>
-            </div>
-          </>
-        )}
+            )}
+          </div>
+        </>
       </section>
     </>
   );
